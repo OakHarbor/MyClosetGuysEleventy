70da17dbb09ba62c699c4c95f914842b2eb37c78
OakHarbor <ohwdinfo@gmail.com>
2023-09-02 13:21:03 -0700

    first commit

---
+++ src/assets/js/nav.js
@@ -0,0 +1,18 @@
+//
+//    Toggle Mobile Navigation
+//
+const navbarMenu = document.querySelector("#navigation #navbar-menu");
+const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
+
+hamburgerMenu.addEventListener('click', function() {
+    navbarMenu.classList.toggle("open");
+    hamburgerMenu.classList.toggle("clicked");
+});
+
+const dropDowns = Array.from(document.querySelectorAll('.cs-dropdown'));
+for (const item of dropDowns) {
+    const onClick = () => {
+    item.classList.toggle('cs-active')
+}
+item.addEventListener('click', onClick)
+}
